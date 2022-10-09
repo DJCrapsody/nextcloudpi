a8fe0d6b057641df42276221b02f690fad9b3877
thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>
2022-10-09 19:59:48 +0200

    install.sh: Make sure nc-datadir is available even if running on unsupported platform
    Signed-off-by: thecalcaholic <6317548+theCalcaholic@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -69,10 +69,12 @@ install_app    ncp.sh
 run_app_unsafe bin/ncp/CONFIG/nc-init.sh
 echo 'Moving data directory to a more sensible location'
 mkdir -p /opt/ncdata
-(
-  export CFGDIR=etc/ncp-config.d
-  run_app_unsafe bin/ncp/CONFIG/nc-datadir.sh
-)
+[[ -f "/usr/local/etc/nc-datadir.cfg" ]] || {
+  should_rm_datadir_cfg=true
+  cp etc/ncp-config.d/nc-datadir.cfg /usr/local/etc/nc-datadir.cfg
+}
+run_app_unsafe bin/ncp/CONFIG/nc-datadir.sh
+[[ -z "$should_rm_datadir_cfg" ]] || rm /usr/local/etc/nc-datadir.cfg
 rm /.ncp-image
 
 # skip on Armbian / Vagrant / LXD ...
