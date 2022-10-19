a56700f5d4452b2ac435b0ccc662b97b98c48689
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2022-10-19 18:23:21 +0200

    install.sh: Fix config directory path for nc-datadir.cfg
    Signed-off-by: Tobias K <6317548+theCalcaholic@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -69,12 +69,12 @@ install_app    ncp.sh
 run_app_unsafe bin/ncp/CONFIG/nc-init.sh
 echo 'Moving data directory to a more sensible location'
 mkdir -p /opt/ncdata
-[[ -f "/usr/local/etc/nc-datadir.cfg" ]] || {
+[[ -f "/usr/local/etc/ncp-config.d/nc-datadir.cfg" ]] || {
   should_rm_datadir_cfg=true
-  cp etc/ncp-config.d/nc-datadir.cfg /usr/local/etc/nc-datadir.cfg
+  cp etc/ncp-config.d/nc-datadir.cfg /usr/local/etc/ncp-config.d/nc-datadir.cfg
 }
 run_app_unsafe bin/ncp/CONFIG/nc-datadir.sh
-[[ -z "$should_rm_datadir_cfg" ]] || rm /usr/local/etc/nc-datadir.cfg
+[[ -z "$should_rm_datadir_cfg" ]] || rm /usr/local/etc/ncp-config.d/nc-datadir.cfg
 rm /.ncp-image
 
 # skip on Armbian / Vagrant / LXD ...
